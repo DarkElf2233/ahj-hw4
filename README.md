878c32cbed2fd77506c9f8e8fa39052c80733f67
DarkElf2233 <fd.melnikoff@gmail.com>
2024-03-10 14:11:39 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Домашнее задание к занятию "4.Организация тестирования"
+
+## Credit Card Validator
+
+
+
+You can check the latest verision of project running [here](https://darkelf2233.github.io/ahj-hw4_1)
---
+++ src/js/app.js
@@ -0,0 +1,49 @@
+import checkCard from './widget/card-number-validator';
+import getCardType from './widget/card-type-validator';
+
+const btn = document.querySelector('.btn');
+const input = document.querySelector('.text-field');
+const success = document.querySelector('.success');
+const fail = document.querySelector('.fail');
+
+const cardsIcons = document.querySelector('.card-icons');
+const cards = [...cardsIcons.querySelectorAll('.card-img')];
+
+btn.addEventListener('click', () => {
+  if (checkCard(input.value)) {
+    success.classList.remove('hide');
+    if (!fail.classList.contains('hide')) {
+      fail.classList.add('hide');
+    }
+  } else {
+    fail.classList.remove('hide');
+    if (!success.classList.contains('hide')) {
+      success.classList.add('hide');
+    }
+  }
+});
+
+input.addEventListener('change', () => {
+  const cardType = getCardType(input.value);
+  if (cardType !== 'Unknown') {
+    for (const card of cards) {
+      if (card.classList.contains('grey-background')) {
+        continue;
+      } else {
+        card.classList.add('grey-background');
+      }
+    }
+    console.log(cardType);
+    const currentCard = document.querySelector(`#card-${cardType}`);
+    console.log(currentCard);
+    currentCard.classList.remove('grey-background');
+  } else {
+    for (const card of cards) {
+      if (card.classList.contains('grey-background')) {
+        card.classList.remove('grey-background');
+      } else {
+        continue;
+      }
+    }
+  }
+});
